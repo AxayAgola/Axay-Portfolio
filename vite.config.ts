4c4e093d2b0ad7ca279cc5bc5dd8360b937b39be
MR.Perfect <146311148+AxayAgola@users.noreply.github.com>
2025-07-08 10:27:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+   build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000 KB
+  },
   plugins: [react()],
   resolve: {
     alias: {
